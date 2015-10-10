40f2c56b43f4044a9ab23f5fb605a5c08f3fd3f3
Louis Popi <theh2o64@gmail.com>
2015-10-10 12:42:58 +0200

    common : Disable camera_test 	* We won't use it anyway

--- common.mk
+++ common.mk
@@ -281,7 +281,7 @@ LIBCAMERA += libmmcamera_interface2
 LIBCAMERA += libmmjpeg_interface
 LIBCAMERA += libqomx_core
 LIBCAMERA += mm-qcamera-app
-LIBCAMERA += camera_test
+#LIBCAMERA += camera_test
 LIBCAMERA += org.codeaurora.camera
 
 #LIBCOPYBIT
