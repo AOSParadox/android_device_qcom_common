2782225fc404f6fa45b6991db4ac5bb91a37682a
Louis Popi <theh2o64@gmail.com>
2016-01-01 12:37:50 +0100

    common: Use new SMS AOSP app
    Change-Id: Iad41e56f42f3371a75a25fc12c6e71de25d50e64
    Signed-off-by: Louis Popi <theh2o64@gmail.com>
    Signed-off-by: theh2o64@gmail.com <theh2o64@gmail.com>
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- base.mk
+++ base.mk
@@ -320,6 +320,7 @@ LAUNCHER += Launcher3
 LAUNCHER += libemoji
 LAUNCHER += Browser
 LAUNCHER += libsepol
+LAUNCHER += messaging
 
 #LIB_NL
 LIB_NL := libnl_2
@@ -651,7 +652,6 @@ PRODUCT_PACKAGES := \
     Email \
     Gallery2 \
     LatinIME \
-    Mms \
     Music \
     Phone \
     Provision \
