638aca68f22604f139d7ff21c951ead0d3cafb8e
Louis Popi <theh2o64@gmail.com>
2016-01-01 12:19:50 +0100

    common: Build Launcher3
    Change-Id: Iacb87b82b266d246692e7b8b8a2c74304c60aacb
    Signed-off-by: Louis Popi <theh2o64@gmail.com>
    Signed-off-by: Louis Popi <theh2o64@gmai.com>
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- base.mk
+++ base.mk
@@ -18,7 +18,7 @@ QCOM_BOARD_PLATFORMS += msm8909w
 QSD8K_BOARD_PLATFORMS := qsd8k
 
 TARGET_USE_VENDOR_CAMERA_EXT := true
-ANDROID_COMPILE_WITH_JACK := false
+ANDROID_COMPILE_WITH_JACK := true
 
 #List of targets that use video hw
 MSM_VIDC_TARGET_LIST := msm8974 msm8610 msm8226 apq8084 msm8916 msm8994 msm8909
@@ -313,7 +313,7 @@ KS += qcks
 KS += efsks
 
 #LAUNCHER
-LAUNCHER := Launcher2
+#LAUNCHER := Launcher2
 LAUNCHER += Launcher3
 LAUNCHER += libemoji
 LAUNCHER += Browser
